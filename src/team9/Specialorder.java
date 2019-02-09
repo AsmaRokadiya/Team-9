cb2d24b51c8b28a8ee2e7dea981206d62bce239c
vinay <vinayraodugyala99@gmail.com>
2019-02-09 11:14:33 -0500

    Specialorder created

---
+++ src/team9/Specialorder.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9;
+import java.util.*;
+/**
+ *
+ * @author temp
+ */
+public class Specialorder {
+    Date date;
+    String number;
+   
+    
+     public void confirm()
+    {
+        System.out.println("order confirm");
+    }
+     
+     public void close()
+    {
+        System.out.println("order close");
+    }
+      
+ public void dispatch()
+    {
+        System.out.println("order dispatch");
+    }
+}
+     
