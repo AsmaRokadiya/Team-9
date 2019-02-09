cac76377e515a74bfef45dde844177c2283e1509
Prashanth <prashanthbashetty@gmail.com>
2019-02-09 10:48:17 -0500

    Order class created

---
+++ src/team9/NormalOrder.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9;
+import java.util.Date;
+/**
+
+ * @author temp
+ */
+public class NormalOrder {
+    Date date1;
+    String number1;
+    public void confirm()
+    {
+        System.out.println("confirm");
+        
+    }
+public void close()
+{
+    System.out.println("close");
+    
+}
+public void dispatch()
+{
+    System.out.println("dispatch");
+    
+}
+public void receive()
+{
+    System.out.println("receive");
+}
+}
